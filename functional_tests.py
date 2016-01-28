be511b6944406e9327709d38fed03e5d12ebd399
Paul Bryant <paulwilliambryant@gmail.com>
2016-01-28 15:27:14 -0600

    add helper method to check for results in results table in FT

--- functional_tests.py
+++ functional_tests.py
@@ -8,8 +8,16 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+
     def tearDown(self):
         self.browser.quit()
+
+
+    def check_for_row_in_results_table(self,row_text):
+        table = self.browser.find_element_by_id('id_results_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_enter_search_term_and_retrieve_results(self):
         #User wants to find all documents that Waterborne has
@@ -34,23 +42,17 @@ class NewVisitorTest(unittest.TestCase):
 
         #User hits ENTER and  the page is updated with the returned results. 
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_results_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('atrazine result', [row.text for row in rows])
-                
+        self.check_for_row_in_results_table('atrazine result')
 
         #Page Title and search bar are still there. User wants to search
         #for all papers regarding Iowa, because Waterborne carries out 
         #a lot of research there. They enter 'Iowa' and hit enter
         inputbox = self.browser.find_element_by_id('id_search_term')
         inputbox.send_keys('Iowa')
-        inputbox.send_keys(Keys.ENTER)
-        
+        inputbox.send_keys(Keys.ENTER) 
+
         #The page updates again with the new results
-        table = self.browser.find_element_by_id('id_results_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('Iowa result',[row.text for row in rows])
+        self.check_for_row_in_results_table('Iowa result')
 
         #The User sees a document they are interested in and so they
         #click on a result, whereupon the document is downloaded to their
