dfb0126f801b256cff3995ac9e32c2c5d4caa799
Paul Bryant <paulwilliambryant@gmail.com>
2016-01-28 12:49:09 -0600

    Basic view now returns miminal HTML

--- docfinder/tests.py
+++ docfinder/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from docfinder.views import home_page
+
 
 # Create your tests here.
 
@@ -9,3 +12,12 @@ class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html'))
+        self.assertIn(b'<title>Search Documents</title>',response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
+        
