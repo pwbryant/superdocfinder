5397ab3184a0ff1f40178a4dd35ee5dc9e31cf66
Paul Bryant <paulwilliambryant@gmail.com>
2016-02-05 14:31:17 -0600

    added UT tests for Searches Results and Documents models

--- docfinder/tests.py
+++ docfinder/tests.py
@@ -25,7 +25,7 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(),expected_html)
       
 
-class SeachesModelTest(TestCase):
+class ModelTest(TestCase):
     
     def test_saving_and_retrieving_search(self):
         first_search = Search()
@@ -43,7 +43,45 @@ class SeachesModelTest(TestCase):
         second_saved_search = saved_searches[1]
         self.assertEqual(first_saved_search.search_terms, 'atrazine')
         self.assertEqual(second_saved_search.search_terms, 'missouri')
+     
+    def test_saving_and_retrieving_searches(self):
+        search = Search(search_terms = 'atrazine')
+        search.save()
+        searches = Searches(search_id = search)
+        searches.save()
+        saved_searches = Searches.objects.all()
+        saved_search = saved_searches[0]
+        self.assertEqual(saved_searches.count(),1)
+        self.assertEqual(saved_search.search_id.search_terms,search.search_terms)
+
+    def test_saving_and_retrieving_documents(self):
+        document = Document(doc_id = '1', filename='test.pdf', author="Slick Willy",abstract='here is an abstract')
+        document.save()
+        saved_document = Document.objects.first()
+        self.assertEqual(saved_document.pk,document.pk)
+
+
+    def test_saving_and_retrieving_results(self):
+        search = Search(search_terms = 'atrazine')
+        search.save()
+        searches = Searches(search_id = search)
+        searches.save()
+        document = Document(doc_id = '1', filename='test.pdf', author="Slick Willy",abstract='here is an abstract')
+        document.save()
         
+        result = Result(doc_id = document, searches_id = searches)
+        result.save()
+
+        results = Result.objects.all()
+        saved_result = results[0]
+
+        self.assertEqual(results.count(),1)
+        self.assertEqual(saved_result.pk,result.pk)
+        self.assertEqual(saved_result.doc_id.pk, document.pk)
+        self.assertEqual(saved_result.searches_id.pk,searches.pk)
+        self.assertEqual(saved_result.searches_id.search_id.pk,search.pk)
+
+
 
 class SearchResultsTests(TestCase):
     
