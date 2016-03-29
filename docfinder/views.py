5f8922ba11b6724dfec566945f61f5b906c55ed2
Paul Bryant <paulwilliambryant@gmail.com>
2016-03-29 14:19:09 -0500

    added highlith functionalit to views and search.html as well as boosted title in solr search. Changes not tested. FT and UT's need to be updated.

--- docfinder/views.py
+++ docfinder/views.py
@@ -42,9 +42,9 @@ def get_search_results(request,search_terms):
     results_ordering = search_terms[-1]
     search_terms_for_sql = search_terms[:-2].replace('_',' ')
     search_terms_for_solr = search_terms[:-2].replace('_','+')
-    solr_search = '"%s"^3 OR partialSearch:%s^2 OR %s' %(search_terms_for_solr,search_terms_for_solr,search_terms_for_solr)
+    solr_search = 'title_sf:%s*^6 OR "%s"^3 OR partialSearch:%s^2 OR %s' % (search_terms_for_solr,search_terms_for_solr,search_terms_for_solr,search_terms_for_solr)
     solr = pysolr.Solr('http://localhost:8983/solr/testcore',timeout=10)
-    results = solr.search(solr_search).__dict__['docs']
+    results = solr.search(solr_search,**{'hl':'true','hl.fl':'doc_body_sf','hl.snippets':5,'hl.fragsize':10}).__dict__['docs']
     search = Search.objects.get(search_terms = search_terms_for_sql)
     searches = Searches.objects.create(search_id = search)
     if len(results) > 0:
@@ -66,17 +66,21 @@ def display_results(request, search_terms):
     results_ordering = search_terms[-1]
     search_terms_for_display = search_terms[:-2].replace('_',' ')
     search_terms_for_solr = search_terms[:-2].replace('_','+')
-    solr_search = '"%s"^3 OR partialSearch:%s^2 OR %s' %(search_terms_for_solr,search_terms_for_solr,search_terms_for_solr)
+    solr_search = 'title_sf:%s*^6 OR "%s"^3 OR partialSearch:%s^2 OR %s' % (search_terms_for_solr,search_terms_for_solr,search_terms_for_solr,search_terms_for_solr)
+
 
     solr = pysolr.Solr('http://localhost:8983/solr/testcore',timeout=10)
+    highlight_params = {'hl':'true','hl.fl':'doc_body_sf','hl.snippets':5,'hl.fragsize':40,'hl.simple.pre':'<b>','hl.simple.post':'</b>'}
     if results_ordering == '2':
-        results = solr.search(solr_search,sort='year desc').__dict__['docs']
+        results = solr.search(solr_search,sort='year desc',**highlight_params).__dict__
     else:
-        results = solr.search(solr_search).__dict__['docs']
-    for result in results:
+        results = solr.search(solr_search,**highlight_params).__dict__
+    for result in results['docs']:
+        result['highlighting'] = ''
         for key in result.keys():
             if type(result[key]) == list:
                 result[key] = result[key][0]
+            result['highlighting'] = '<br>'.join(results['highlighting'][result['id']]['doc_body_sf'])
     return render(request,'search.html',
             {'search_results':results,'search_terms':search_terms_for_display,'form':SearchForm(initial={'choice_field':results_ordering})})
 
